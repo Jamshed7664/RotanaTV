16f3da42c1117228dd084db5426de8437de28973
jamshed7664 <jamshed7664@gmail.com>
2021-01-13 18:29:33 +0530

    Add home screen

--- home/home.js
+++ home/home.js
@@ -1,3 +1,18 @@
 function gotoVideo(){
 	 window.location.href = "../video/video.html";
 }
+
+var span = document.getElementById('clock');
+
+function time() {
+  var d = new Date();
+  var s = d.getSeconds();
+  var m = d.getMinutes();
+  var h = d.getHours();
+	  document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+  console.log(d,m,h)
+}
+
+setInterval(time, 1000);
+
+window.onload = time();
